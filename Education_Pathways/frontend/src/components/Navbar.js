46247bb51eecb44b3e5d1f1d0f2ad4b222c0a6a8
Jonathan Yan <jyan0302@gmail.com>
2022-09-18 00:55:52 -0400

    Undid change to tab title and change title of main page to my name

--- Education_Pathways/frontend/src/components/Navbar.js
+++ Education_Pathways/frontend/src/components/Navbar.js
@@ -51,7 +51,7 @@ export default class NavbarComp extends Component {
             <Navbar.Brand>
               <img src={logo} alt="" />{" "}
               <Nav.Link href="/" style={{ color: "white", display: "inline" }}>
-                EP-Ho Jonathan Yan
+                Education Pathways
               </Nav.Link>
             </Navbar.Brand>
 
